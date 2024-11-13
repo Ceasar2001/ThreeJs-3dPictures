6251387a914eca0bc0d04a393f923aa558235023
IanCodes <ecleoceasar1@gmail.com>
2024-11-13 13:17:38 +0800

    commit 3

--- main.js
+++ main.js
@@ -30,6 +30,8 @@ const artist = [
     'ceasar',
 ];
 
+const textureLoader = new THREE.TextureLoader();
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
@@ -45,14 +47,44 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 
 // camera.position.z = 6;
 
-const artwork = new THREE.Mesh(
-    new THREE.BoxGeometry(3, 2, 0.1),
-    new THREE.MeshBasicMaterial({ color: 0xf08080})
-);
-artwork.position.z = -4;
-scene.add(artwork);
+const rootNode = new THREE.Object3D();
+scene.add(rootNode);
+
+let count = 6;
+for(let i = 0; i < count; i++){
+
+    const texture = textureLoader.load(images[i]);
+
+    texture.colorSpace = THREE.SRGBColorSpace;
+
+    const baseNode = new THREE.Object3D();
+    baseNode.rotation.y = i * ( 2 * Math.PI / count );
+    rootNode.add(baseNode);
+
+    const border = new THREE.Mesh(
+        new THREE.BoxGeometry(3.2, 2.2, 0.09),
+        new THREE.MeshBasicMaterial({ color: 0x202020 })
+    );
+    border.position.z = -4;
+    baseNode.add(border);
+
+    const artwork = new THREE.Mesh(
+        new THREE.BoxGeometry(3, 2, 0.1),
+        new THREE.MeshStandardMaterial({ map: texture })
+    );
+    artwork.position.z = -4;
+    baseNode.add(artwork);
+}
+
+const spotlight = new THREE.SpotLight(0xffffff, 100.0, 10.0, 0.65, 1);
+spotlight.position.set(0, 5, 0);
+spotlight.target.position.set(0, 0.6, -5);
+scene.add(spotlight);
+scene.add(spotlight.target);
 
 function animate() {
+
+    rootNode.rotation.y += 0.02;
 
 	// cube.rotation.x += 0.01;
 	// cube.rotation.y += 0.01;
